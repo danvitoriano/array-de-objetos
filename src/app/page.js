50ddd5961036c90f14eab0d692c62b874793ed3e
Dan Vitoriano <vitoriano08@gmail.com>
2023-11-21 10:20:22 -0300

    add new item

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,49 @@
 export default function Home() {
-
-  const arrayDeObjetos = [{
-    rm: 1243,
-    nome: 'Danilo',
-    idade: 42
-  }, {
-    rm: 1244,
-    nome: 'Diego',
-    idade: 20
-  }, {
-    rm: 1245,
-    nome: 'Mariana',
-    idade: 22
-  },
-  {
-    rm: 1246,
-    nome: 'Roberto',
-    idade: 22
-  },
-];
+  const arrayDeObjetos = [
+    {
+      rm: 1243,
+      nome: "Danilo",
+      idade: 42,
+    },
+    {
+      rm: 1244,
+      nome: "Diego",
+      idade: 20,
+    },
+    {
+      rm: 1245,
+      nome: "Mariana",
+      idade: 22,
+    },
+    {
+      rm: 1246,
+      nome: "Roberto",
+      idade: 22,
+    },
+    {
+      rm: 1247,
+      nome: "Fernanda",
+      idade: 22,
+    },
+    {
+      rm: 1248,
+      nome: "Gabriel",
+      idade: 22,
+    },
+  ];
 
   return (
     <main>
       <h1>Home</h1>
       <ul>
-        {
-          arrayDeObjetos.map(
-            function(objeto) {
-              return (<li key={objeto.rm}>{objeto.nome} - {objeto.idade}</li>)
-            }
-          )
-        }
+        {arrayDeObjetos.map(function (objeto) {
+          return (
+            <li key={objeto.rm}>
+              {objeto.nome} - {objeto.idade}
+            </li>
+          );
+        })}
       </ul>
     </main>
-  )
-};
+  );
+}
